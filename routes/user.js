45114fe3a4c210d57d072284e395331c04e77817
tyoprataama <tyoprataama004@gmail.com>
2022-05-29 20:26:01 +0700

    Build user statistic

--- routes/user.js
+++ routes/user.js
@@ -42,4 +42,41 @@ router.get('/find/:id', verifyTokenAdmin, async (request, response) => {
   }
 });
 
+//  Get all users
+router.get('/', verifyTokenAdmin, async (request, response) => {
+  const query = request.query.new;
+  try {
+    //  If there is new query, return (2) latest users
+    const user = query ? await User.find().sort({ _id: -1 }).limit(2) : await User.find();
+    return response.status(200).json(user);
+  } catch (error) {
+    return response.status(500).json(error);
+  }
+});
+
+//  Get user statistic
+router.get('/stats', verifyTokenAdmin, async (request, response) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: '$createdAt' },
+        },
+      },
+      {
+        $group: {
+          _id: '$month', // Month number from user sign up
+          total: { $sum: 1 }, // Total user that month
+        },
+      },
+    ]);
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
+
 module.exports = router;
